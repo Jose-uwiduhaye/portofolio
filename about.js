a3cb55777d9eb3ab7abb4097828119bd537a5f15
uwiduhaye1 <joseuwiduhayee@gmail.com>
2024-01-03 12:22:38 +0200

    first commit

---
+++ about.js
@@ -0,0 +1,25 @@
+// function message(){
+//     var Name=document.getElementById('name');
+//     var phone=document.getElementById('phone');
+//     var  message=document.getElementById('message');
+//     const success=document.getElementById('success');
+//     const danger=Document.getElementById('danger');
+//     if(Name.value === ''||phone.value === ' '||message.value==='')
+//     {
+//         danger.style.display='block';
+//     }
+//     else{
+//         setTimeout(()=>{
+
+//             Name.value='';
+//             phone.value='';
+//             message.value='';
+//         },2000);
+//         success.style.display='block';
+//     }
+//     setTimeout(()=>{
+
+//         danger.style.display='none';
+//         success.style.display='none';
+//         },4000);
+// }
---
+++ home.js
@@ -0,0 +1,10 @@
+// ScrollReveal({
+//     reset:true,
+//     distance:'80px',
+//     duration:2000,
+//     delay:L200
+// });
+// ScrollReveal().reveal('home-content,.heading',{origin:'top'});
+// ScrollReveal().reveal('.home-img, .heading, .portfolio-Box, .contact form',{origin:'top'})
+
+
